e1593862e2e60601cab3556d417899f253741c04
Iflar <classicsience@gmail.com>
2022-11-20 14:37:14 -0500

    added hero interface + user imput into the heroes.component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero"
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Bilbo Baggins' },
+    { id: 13, name: 'Yogmahashi' },
+    { id: 14, name: 'Demonkiller' },
+    { id: 15, name: 'Maystr shiif' },
+    { id: 16, name: 'laird of the wring' },
+    { id: 17, name: 'Frodo' },
+    { id: 18, name: 'Iflar' },
+    { id: 19, name: 'Manao' },
+    { id: 20, name: 'Velthro' },
+];
